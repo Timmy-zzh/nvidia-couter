a98a847c4a7cc0b91606c3016e70626f64dedbcb
yanlei <yanlei@yingzi.com>
2024-07-04 14:07:01 +0800

    CMake编译配置

---
+++ build-aarch64/CMakeFiles/nvidia-couter-cmake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nvidia-couter-cmake.dir/main.cpp.o"
+  "nvidia-couter-cmake"
+  "nvidia-couter-cmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nvidia-couter-cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
